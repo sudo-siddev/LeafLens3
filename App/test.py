372c820dc956db7a9d1db6903ab4fd379cc6fae7
Siddharth <116259242+sudo-siddev@users.noreply.github.com>
2026-01-30 21:11:20 +0530

    Change number of classes for ResNet50 model

--- App/test.py
+++ App/test.py
@@ -2,6 +2,6 @@ import torch
 import torchvision.models as models
 import torch.nn as nn
 resnet50 = models.resnet50()
-num_classes = 39
+num_classes = 18
 state_dict = torch.load("ResNet50.pt")
 resnet50.fc = nn.Linear(resnet50.fc.in_features, num_classes)
